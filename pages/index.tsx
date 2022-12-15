c30c7da617a80230adc01eb8f68080beac6cbd1e
Henok Hailemariam <henokhm2@gmail.com>
2022-12-15 18:02:38 +0300

    Initial commit: Project setup

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Chingu Solo Project - Journal App</title>
+        <meta
+          name="description"
+          content="This is a simple journal app for my first Chingu solo project."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="min-w-screen pt-4 text-center text-3xl font-bold underline">
+          Hello Chingu!
+        </h1>
+      </main>
+    </>
+  );
+}
