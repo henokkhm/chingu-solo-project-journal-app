3b4a73f2225a8240df1cae893f42c12b79a94fd1
Henok Hailemariam <henokhm2@gmail.com>
2022-12-24 23:11:23 +0300

    Move Head component to Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useSnackbar } from "../context/snackbar-context";
 import Snackbar from "./snackbar";
 
@@ -9,6 +10,15 @@ function Layout({ children }: LayoutProps) {
   const { snackbar } = useSnackbar();
   return (
     <>
+      <Head>
+        <title>Chingu Solo Project - Journal App</title>
+        <meta
+          name="description"
+          content="This is a simple journal app for my first Chingu solo project."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <>{children}</>
       {snackbar.message.length > 0 && <Snackbar {...snackbar} />}
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Header from "../components/header";
 import JournalForm from "../components/forms/journal-form";
 
@@ -8,15 +7,6 @@ import JournalsWrapper from "../components/journals-wrapper";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Chingu Solo Project - Journal App</title>
-        <meta
-          name="description"
-          content="This is a simple journal app for my first Chingu solo project."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header pageTitle={"Create a Note"} />
       <main className="flex flex-col gap-16">
         <p className="pb-6 leading-6 text-gray-800">
