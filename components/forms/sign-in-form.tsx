1187f24b449128f4e7ed7719645c201754dd9dfc
Henok Hailemariam <henokhm2@gmail.com>
2022-12-24 23:08:07 +0300

    Add Formik and Yup validation to sign-in form

--- components/forms/sign-in-form.tsx
+++ components/forms/sign-in-form.tsx
@@ -1,22 +1,52 @@
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import PrimaryButton from "../buttons/primary-button";
 
 function SignInForm() {
   return (
-    <form className="form-styles max-w-sm">
-      <div>
-        <label htmlFor="user_name" className="label-styles">
-          User name:
-        </label>
-        <input type="text" id="user_name" className="input-styles" />
-      </div>{" "}
-      <div>
-        <label htmlFor="password" className="label-styles">
-          Password:
-        </label>
-        <input type="password" id="password" className="input-styles" />
-      </div>
-      <PrimaryButton text="Sign In" />
-    </form>
+    <Formik
+      initialValues={{ userName: "", password: "" }}
+      validationSchema={Yup.object({
+        userName: Yup.string().required("A username is required"),
+        password: Yup.string().required("A password is required"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="form-styles max-w-sm">
+          <div>
+            <label htmlFor="userName" className="label-styles">
+              User Name
+            </label>
+            <Field name="userName" type="text" className="input-styles" />
+            <ErrorMessage
+              component="div"
+              className="py-2 text-sm text-red-700"
+              name="userName"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password" className="label-styles">
+              Password
+            </label>
+            <Field name="password" type="password" className="input-styles" />
+            <ErrorMessage
+              component="div"
+              className="py-2 text-sm text-red-700"
+              name="password"
+            />
+          </div>
+
+          <PrimaryButton loading={isSubmitting} type="submit" text="Sign Up" />
+        </Form>
+      )}
+    </Formik>
   );
 }
 
